807554c31e4bcd7654715a4216db7b9f344e50ee
jhs1255 <happyrich15142@gmail.com>
2023-10-18 16:08:39 +0900

    example1

--- pipe_ex.c
+++ pipe_ex.c
@@ -1,57 +1,65 @@
-#include <stdio.h>
+#include <unistd.h>
 #include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
-#define N 4
+
+#define N 5
 #define MAXLINE 100
-void sinx_taylor(int num_elements, int terms, double* x, double* result)
-{
+
+void sinx_taylor(int num_elements, int terms, double* x, double* result){
+	int pid[N];
 	int fd[2*N];
-	char message[MAXLINE],line[MAXLINE];
-	int length;
-	for(int i=0;i<num_elements;i++){
-		int* p_id = &fd[2*i];
-		pipe(p_id);
+	int* p_fd = fd;
+	for(int i=0; i<N; i++) {
+            pipe(p_fd);
+	    p_fd += 2;
 	}
-	int pid;
-	for(int i=0; i<num_elements; i++) {
-		if((pid=fork())==0){
+	
+	int length;
+	char message[MAXLINE];
+	char line[MAXLINE];
+
+	for(int i=0; i<num_elements; i++){
+		p_fd = &fd[2*i];
+		if((pid[i] = fork()) == 0){
+			close(p_fd[0]);
+
 			double value = x[i];
 			double numer = x[i] * x[i] * x[i];
-			double denom = 6.; // 3!
+			double denom = 6;
 			int sign = -1;
-			for(int j=1; j<=terms; j++) {
+
+			for(int j=1; j<=terms; j++){
 				value += (double)sign * numer / denom;
 				numer *= x[i] * x[i];
 				denom *= (2.*(double)j+2.) * (2.*(double)j+3.);
 				sign *= -1;
 			}
-			result[i] = value;
-			sprintf(message,"%lf",result[i]);
-			length=strlen(message)+1;
-			write(pid[1],message,length);
-			exit(my_id);
 
+			result[i] = value;
+			sprintf(message, "%lf\n", result[i]);
+			length = strlen(message)+1;
+			write(p_fd[1], message, length); 
+			exit(i);
 		}else{
-			close(pid[1]);
+			close(p_fd[i]);
 		}
 	}
-	int* stat;
-	for(int i=0;i<num;i++){
+	int stat;
+	for(int i=0; i<N; i++) {
 		wait(&stat);
-		int child_id = stat >> 8;
-		read(fd[2*child_id],line,MAXLINE);
 
-		double res = atof(line);
-		printf("%d %lf%s\n",child_id,res,line);
-		result[child_id]=res;
+		int child_id = stat >> 8;
+		read(fd[child_id*2+0], line, MAXLINE);
+		result[child_id] = atof(line);
 	}
 }
 
 int main()
 {
-	double x[N] = {0, M_PI/6., M_PI/3., 0.134};
+	double x[N] = {0, M_PI/6.f, M_PI/3.f, 0.134};
 	double res[N];
 	sinx_taylor(N, 3, x, res);
 	for(int i=0; i<N; i++) {
